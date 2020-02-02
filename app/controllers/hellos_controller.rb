d6f37aeb0653dca6d047b5b1225ab2cb84af23a8
rvillage <rvillage@gmail.com>
2020-02-02 22:41:07 +0900

    :sparkles: Add hello_vue sample

---
+++ app/controllers/hellos_controller.rb
@@ -0,0 +1,4 @@
+class HellosController < ApplicationController
+  def show
+  end
+end
---
+++ app/javascript/hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="hello">
+    {{ message }}
+    <app />
+  </div>
+</template>
+
+<script>
+import App from './app.vue'
+
+export default {
+  components: { App },
+  data: () => {
+    return {
+      message: 'Can you say hello?'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
