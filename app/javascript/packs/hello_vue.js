2b98e8b9e756de1b4dbf16588bbeeb2da7d54e9e
rvillage <rvillage@gmail.com>
2020-02-06 01:50:12 +0900

    :shower: Remove sample vue files

--- app/javascript/packs/hello_vue.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import Hello from '../hello.vue'
-
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    el: '#hello',
-    render: h => h(Hello)
-  })
-})
