9b8b513c9bcb403f56b0cdaf6063c356bf486cde
rvillage <rvillage@gmail.com>
2020-03-08 23:39:47 +0900

    :sparkles: Add sass loader setting

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,12 +4,14 @@ const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 const pug = require('./loaders/pug')
 const typescript = require('./loaders/typescript')
+const sass = require('./loaders/sass')
 const aliasConfig = require('./configs/alias')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 environment.loaders.prepend('pug', pug)
 environment.loaders.prepend('typescript', typescript)
+environment.loaders.prepend('sass', sass)
 
 environment.config.merge(aliasConfig)
 
